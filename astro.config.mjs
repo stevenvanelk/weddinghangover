5e8fb385c68028377234dddb1fde3a16ab1a68ae
stevenvanelk <srvanelk@gmail.com>
2025-07-23 01:41:45 -0400

    Update Site in Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://stevenvanelk.github.io/weddinghangover/',
   base: '/weddinghangover/',
   integrations: [mdx()],
 });
