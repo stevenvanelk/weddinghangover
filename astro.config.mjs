e8c9fcff55270c3dd4a8a23b20e51467fa44f03d
stevenvanelk <srvanelk@gmail.com>
2025-07-30 15:26:28 -0400

    robots.txt and sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://weddinghangover.com',
   base: '/',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
 
